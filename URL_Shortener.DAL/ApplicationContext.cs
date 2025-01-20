611083afbec7c6459735abb0367be413209329ef
Nedih <ihor.nedaiev@gmail.com>
2025-01-20 18:45:44 +0200

    Project architecture configured

---
+++ URL_Shortener.DAL/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿namespace URL_Shortener.DAL
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using URL_Shortener.DAL.Entities;
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+
+        public string DbPath { get; }
+
+        public ApplicationContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "shortener.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ URL_Shortener.DAL/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URL_Shortener.DAL.Entities
+{
+    public class Entity
+    {
+        public int EntityId { get; set; }
+    }
+}
