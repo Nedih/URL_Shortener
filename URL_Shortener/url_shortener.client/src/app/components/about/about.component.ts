74a61f02805076fe4057d24cb6f73dc20cb85f06
Nedih <ihor.nedaiev@gmail.com>
2025-01-25 18:12:37 +0200

    About Page UI abjustments

--- URL_Shortener/url_shortener.client/src/app/components/about/about.component.ts
+++ URL_Shortener/url_shortener.client/src/app/components/about/about.component.ts
@@ -3,20 +3,35 @@ import { environment } from '../../../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss',
-    imports: [FormsModule, NgIf]
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    CdkTextareaAutosize,
+    NgIf
+  ]
 })
 export class AboutComponent {
   userText: string = environment.aboutPageText;
   isEditing: boolean = false;
+  fontSize = '16px';
 
   constructor(private authService: AuthService) { }
 
   toggleEdit(): void {
+    if (this.isAdmin())
     this.isEditing = true;
   }
 
@@ -27,6 +42,10 @@ export class AboutComponent {
 
   cancelEdit(): void {
     this.isEditing = false;
+  }
+
+  triggerResize() {
+    console.log("Font size changed to:", this.fontSize);
   }
 
   isAdmin() {
